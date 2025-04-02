186bfa567d60d6742acf4dedbae7a9246ccf1e2d
Stanley4225 <stanleysakyihyde@trentu.ca>
2025-04-02 16:16:26 -0400

    Singleton design pattern

--- RentalSystem.java
+++ RentalSystem.java
@@ -3,9 +3,23 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class RentalSystem {
+	
+	private static RentalSystem instance;
+	
     private List<Vehicle> vehicles = new ArrayList<>();
     private List<Customer> customers = new ArrayList<>();
     private RentalHistory rentalHistory = new RentalHistory();
+    
+    
+    // Method to get the singleton instance
+    public static synchronized RentalSystem getInstance() {
+        if (instance == null) {
+            instance = new RentalSystem();
+        }
+        return instance;
+    }
+    
+    
 
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
